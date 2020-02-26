5957bfe4b406be0bd61bf27aa000ca30a7851f77
okawibawa <okaa.wibawa@gmail.com>
2020-02-26 16:58:59 +0800

    Adding some new features.

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-let scores, roundScore, dice, activePlayer;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+let rollDice = document.querySelector('.btn-roll');
+let holdDice = document.querySelector('.btn-hold');
+let imgDice = document.querySelector('.dice');
 
-document.querySelector('#score-0').textContent = dice;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+
+imgDice.style.display = 'none';
+
+rollDice.addEventListener('click', function() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  imgDice.style.display = 'block';
+  imgDice.src = 'img/dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+  } else {
+    //  switch player
+  }
+});
+
+holdDice.addEventListener('click', function() {});
