e490f00831976a4741b992a1547e5d00c50a146c
okawibawa <okaa.wibawa@gmail.com>
2020-02-29 01:37:51 +0800

    adding set final score feature

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-let scores, roundScore, activePlayer, gameState;
-
+let scores, roundScore, activePlayer, gameState, goalScore, lastDice;
 let imgDice = document.querySelector('.dice');
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+  // checking if the game states is true
   if (gameState) {
     let dice = Math.floor(Math.random() * 6) + 1;
 
     imgDice.style.display = 'block';
     imgDice.src = 'img/dice-' + dice + '.png';
 
-    if (dice !== 1) {
+    if (dice === 6 && lastDice === 6) {
+      // set the score to 0
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = 0;
+      nextPlayer();
+    } else if (dice !== 1) {
       // adding the value of dice to current round score
       roundScore += dice;
       document.querySelector(
         '#current-' + activePlayer,
       ).textContent = roundScore;
     } else {
+      // if the value is one, change the player to the next one
       nextPlayer();
     }
+
+    lastDice = dice;
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
+  // checking if the game states is true
   if (gameState) {
     // add current score to global score
     scores[activePlayer] += roundScore;
@@ -31,7 +40,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
       scores[activePlayer];
 
     // check the score
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= goalScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 
       // set dice display to none
@@ -72,18 +81,22 @@ function nextPlayer() {
 }
 
 function init() {
+  // initializing and declaring varibales
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
   gameState = true;
 
+  // setting all scores value to 0
   document.getElementById('current-0').textContent = 0;
   document.getElementById('current-1').textContent = 0;
   document.getElementById('score-0').textContent = 0;
   document.getElementById('score-1').textContent = 0;
 
+  // set the dice display to none
   imgDice.style.display = 'none';
 
+  // adding and removing class
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
   document.querySelector('.player-1-panel').classList.remove('active');
@@ -92,4 +105,8 @@ function init() {
 
   document.getElementById('name-0').textContent = 'Player 1';
   document.getElementById('name-1').textContent = 'Player 2';
+}
+
+function getScore() {
+  goalScore = document.querySelector('#setScore').value;
 }
